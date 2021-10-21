b56311f0418dfbe71b165c3aed85489cd2d2acdb
kabilan r <kabilan@clustrex.com>
2021-10-21 12:30:53 +0530

    added auto sn

--- trades.js
+++ trades.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('#trade').DataTable({
         "fnRowCallback" : function(nRow, aData, iDisplayIndex){
-            $("td:first", nRow).html(iDisplayIndex +1);
+            var api = this.api();
+            $("td:first", nRow).html(iDisplayIndex +1 +(api.page.info()['page'] *api.page.info()['length']));
            return nRow;
         },
     });
